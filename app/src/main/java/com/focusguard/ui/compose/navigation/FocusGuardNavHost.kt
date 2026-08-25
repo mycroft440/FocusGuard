73df76cb6a0b55bba65c09c0b28e630a56b482b5
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-25 14:09:28 -0300

    fix: use PointerInputScope await event member

--- app/src/main/java/com/focusguard/ui/compose/navigation/FocusGuardNavHost.kt
+++ app/src/main/java/com/focusguard/ui/compose/navigation/FocusGuardNavHost.kt
@@ -28,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.awaitPointerEventScope
 import androidx.compose.ui.input.pointer.pointerInput
 import com.focusguard.manager.PomodoroManager
 import com.focusguard.focusmode.FocusModeIdleReturnPolicy
