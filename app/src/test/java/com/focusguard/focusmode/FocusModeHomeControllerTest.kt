9e706864326600952e54124f8f2d669439a60bd3
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-25 11:40:55 -0300

    test(focus-mode): run home intent assertions with Robolectric

--- app/src/test/java/com/focusguard/focusmode/FocusModeHomeControllerTest.kt
+++ app/src/test/java/com/focusguard/focusmode/FocusModeHomeControllerTest.kt
@@ -4,7 +4,12 @@ import android.app.admin.DevicePolicyManager
 import android.content.Intent
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class FocusModeHomeControllerTest {
 
     @Test
