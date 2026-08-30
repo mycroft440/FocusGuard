b3a8d6a596a3e39b4dd58b64d34b11881519632f
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-29 21:04:11 -0300

    test: use supported Robolectric SDK 34

--- app/src/test/java/com/focusguard/monetization/RewardedGateStateStoreTest.kt
+++ app/src/test/java/com/focusguard/monetization/RewardedGateStateStoreTest.kt
@@ -13,7 +13,7 @@ import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [35])
+@Config(sdk = [34])
 class RewardedGateStateStoreTest {
     private lateinit var context: Context
 
