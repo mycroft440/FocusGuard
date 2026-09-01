70a745e4781565a93bd4fa3f49a905186eb15b9c
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-01 15:13:25 -0300

    test: cover rewarded gates for biometric and selfie

--- app/src/test/java/com/focusguard/monetization/MonetizationPolicyTest.kt
+++ app/src/test/java/com/focusguard/monetization/MonetizationPolicyTest.kt
@@ -29,10 +29,12 @@ class MonetizationPolicyTest {
     }
 
     @Test
-    fun timeBlockRequiresThreeRewardedAds() {
+    fun rewardedAdCountsMatchCurrentProductRules() {
         assertEquals(1, MonetizationPolicy.FREE_APP_LIMITS)
         assertEquals(1, MonetizationPolicy.FREE_SITE_LIMITS)
         assertEquals(3, MonetizationPolicy.TIME_BLOCK_REWARDED_ADS)
+        assertEquals(1, MonetizationPolicy.BIOMETRIC_UNLOCK_REWARDED_ADS)
+        assertEquals(1, MonetizationPolicy.INTRUDER_SELFIE_REWARDED_ADS)
     }
 
     @Test
@@ -40,10 +42,18 @@ class MonetizationPolicyTest {
         val app = RewardedGateKeys.forRequest("Adicionar mais um aplicativo", 1)
         val site = RewardedGateKeys.forRequest("Adicionar mais um site", 1)
         val time = RewardedGateKeys.forRequest("Ativar bloqueio sem senha", 3)
+        val biometricPt = RewardedGateKeys.forRequest("Desbloquear apps com digital", 1)
+        val biometricEn = RewardedGateKeys.forRequest("Unlock apps with biometric", 1)
+        val selfiePt = RewardedGateKeys.forRequest("Selfie do Intruso", 1)
+        val selfieEn = RewardedGateKeys.forRequest("Intruder Selfie", 1)
 
         assertEquals(RewardedGateKeys.EXTRA_APP_LIMIT_SLOT, app)
         assertEquals(RewardedGateKeys.EXTRA_SITE_LIMIT_SLOT, site)
         assertEquals(RewardedGateKeys.TIME_BLOCK_CREATION, time)
-        assertTrue(setOf(app, site, time).size == 3)
+        assertEquals(RewardedGateKeys.BIOMETRIC_APP_UNLOCK, biometricPt)
+        assertEquals(RewardedGateKeys.BIOMETRIC_APP_UNLOCK, biometricEn)
+        assertEquals(RewardedGateKeys.INTRUDER_SELFIE, selfiePt)
+        assertEquals(RewardedGateKeys.INTRUDER_SELFIE, selfieEn)
+        assertTrue(setOf(app, site, time, biometricPt, selfiePt).size == 5)
     }
 }
