b033183a03e1971d625d73341a985d23664edd69
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-01 15:11:54 -0300

    monetization: require rewarded ads for security extras

--- app/src/main/java/com/focusguard/monetization/MonetizationPolicy.kt
+++ app/src/main/java/com/focusguard/monetization/MonetizationPolicy.kt
@@ -4,6 +4,8 @@ object MonetizationPolicy {
     const val FREE_APP_LIMITS = 1
     const val FREE_SITE_LIMITS = 1
     const val TIME_BLOCK_REWARDED_ADS = 3
+    const val BIOMETRIC_UNLOCK_REWARDED_ADS = 1
+    const val INTRUDER_SELFIE_REWARDED_ADS = 1
 
     fun requiresExtraUsageLimitAd(
         existingConfiguredCount: Int,
