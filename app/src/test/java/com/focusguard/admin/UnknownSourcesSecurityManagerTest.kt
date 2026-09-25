9089b65762a6b3e029d0e5ed3ee41bf8423ac7f0
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-24 21:03:51 -0300

    Fix unknown sources unit test runner

--- app/src/test/java/com/focusguard/admin/UnknownSourcesSecurityManagerTest.kt
+++ app/src/test/java/com/focusguard/admin/UnknownSourcesSecurityManagerTest.kt
@@ -3,10 +3,7 @@ package com.focusguard.admin
 import android.os.UserManager
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 class UnknownSourcesSecurityManagerTest {
 
     @Test
